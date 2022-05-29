c14157b46f04bf52b986c08bfb94bd1ce0a0e06e
jiahoe <mrjiahoe@hotmail.com>
2022-05-29 19:20:21 +0800

    error handling test

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -50,12 +51,14 @@ router.get("/:id", async (req, res) => {
 });
 
 //  GET ALL
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
+	const failed = true;
+
 	try {
 		const hotels = await Hotel.find();
 		res.status(200).json(hotels);
 	} catch (err) {
-		res.status(500).json(err);
+		next(err);
 	}
 });
 
