122e28246db2ef4a38b891c6707673f039f6afa8
jiahoe <mrjiahoe@hotmail.com>
2022-05-30 18:39:02 +0800

    mongodb auth & jwt with cookies

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -19,6 +20,7 @@ const connect = async () => {
 };
 
 // middlewares
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+	deleteUser,
+	getUser,
+	getUsers,
+	updateUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", updateUser);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+router.get("/", getUsers);
 
 export default router;
