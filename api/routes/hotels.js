a3922f4eb50ca8edce534ccc022c351fa81a0168
jiahoe <mrjiahoe@hotmail.com>
2022-05-30 23:17:51 +0800

    get document count

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+	countByCity,
+	countByType,
 	createHotel,
 	deleteHotel,
 	getHotel,
@@ -21,9 +23,11 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 //  GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //  GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
 
 export default router;
