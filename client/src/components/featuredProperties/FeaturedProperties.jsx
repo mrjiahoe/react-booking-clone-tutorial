51c57f1c307264c4e8d191e0af115abb97606287
jiahoe <mrjiahoe@hotmail.com>
2022-06-02 16:50:05 +0800

    fetch data for property list & featured properties

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,64 +1,33 @@
+import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
+	const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+
 	return (
 		<div className="fp">
-			<div className="fpItem">
-				<img
-					src="https://images.unsplash.com/photo-1616047006789-b7af5afb8c20?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="property"
-					className="fpImg"
-				/>
-				<span className="fpName">Apartment de delux</span>
-				<span className="fpCity">iMaginatioN</span>
-				<span className="fpPrice">Starting from $100</span>
-				<div className="fpRating">
-					<button>8.9</button>
-					<span>Excellent</span>
-				</div>
-			</div>
-			<div className="fpItem">
-				<img
-					src="https://images.unsplash.com/photo-1616047006789-b7af5afb8c20?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="property"
-					className="fpImg"
-				/>
-				<span className="fpName">Apartment de delux</span>
-				<span className="fpCity">iMaginatioN</span>
-				<span className="fpPrice">Starting from $100</span>
-				<div className="fpRating">
-					<button>8.9</button>
-					<span>Excellent</span>
-				</div>
-			</div>
-			<div className="fpItem">
-				<img
-					src="https://images.unsplash.com/photo-1616047006789-b7af5afb8c20?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="property"
-					className="fpImg"
-				/>
-				<span className="fpName">Apartment de delux</span>
-				<span className="fpCity">iMaginatioN</span>
-				<span className="fpPrice">Starting from $100</span>
-				<div className="fpRating">
-					<button>8.9</button>
-					<span>Excellent</span>
-				</div>
-			</div>
-			<div className="fpItem">
-				<img
-					src="https://images.unsplash.com/photo-1616047006789-b7af5afb8c20?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="property"
-					className="fpImg"
-				/>
-				<span className="fpName">Apartment de delux</span>
-				<span className="fpCity">iMaginatioN</span>
-				<span className="fpPrice">Starting from $100</span>
-				<div className="fpRating">
-					<button>8.9</button>
-					<span>Excellent</span>
-				</div>
-			</div>
+			{loading ? (
+				"Loading"
+			) : (
+				<>
+					{data.map((item) => (
+						<div className="fpItem" key={item._id}>
+							<img src={item.photos[0]} alt="" className="fpImg" />
+							<span className="fpName">{item.name}</span>
+							<span className="fpCity">{item.city}</span>
+							<span className="fpPrice">
+								Starting from ${item.cheapestPrice}
+							</span>
+							{item.rating && (
+								<div className="fpRating">
+									<button>{item.rating}</button>
+									<span>Excellent</span>
+								</div>
+							)}
+						</div>
+					))}
+				</>
+			)}
 		</div>
 	);
 };
--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,63 +1,39 @@
+import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
+	const { data, loading, error } = useFetch("hotels/countByType");
+
+	const images = [
+		"https://images.unsplash.com/photo-1599722585837-c1cb8eea32ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480",
+		"https://images.unsplash.com/photo-1598474886373-d215fcc1942a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1144",
+		"https://images.unsplash.com/photo-1612089731919-c517c3f158dc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480",
+		"https://images.unsplash.com/photo-1609731169878-93aecfda8779?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480",
+		"https://images.unsplash.com/photo-1608727638571-6b6f64fdc9bc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1483",
+	];
+
 	return (
 		<div className="pList">
-			<div className="pListItem">
-				<img
-					src="https://images.unsplash.com/photo-1599722585837-c1cb8eea32ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="hotel"
-					className="pListImg"
-				/>
-				<div className="pListTitles">
-					<h1>Hotels</h1>
-					<h2>123 hotels</h2>
-				</div>
-			</div>
-			<div className="pListItem">
-				<img
-					src="https://images.unsplash.com/photo-1598474886373-d215fcc1942a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1144"
-					alt="villas"
-					className="pListImg"
-				/>
-				<div className="pListTitles">
-					<h1>Villas</h1>
-					<h2>123 Villas</h2>
-				</div>
-			</div>
-			<div className="pListItem">
-				<img
-					src="https://images.unsplash.com/photo-1612089731919-c517c3f158dc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="Cabins"
-					className="pListImg"
-				/>
-				<div className="pListTitles">
-					<h1>Cabins</h1>
-					<h2>123 Cabins</h2>
-				</div>
-			</div>
-			<div className="pListItem">
-				<img
-					src="https://images.unsplash.com/photo-1609731169878-93aecfda8779?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480"
-					alt="Apartments"
-					className="pListImg"
-				/>
-				<div className="pListTitles">
-					<h1>Apartments</h1>
-					<h2>123 Apartments</h2>
-				</div>
-			</div>
-			<div className="pListItem">
-				<img
-					src="https://images.unsplash.com/photo-1608727638571-6b6f64fdc9bc?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1483"
-					alt="Resorts"
-					className="pListImg"
-				/>
-				<div className="pListTitles">
-					<h1>Resorts</h1>
-					<h2>123 Resorts</h2>
-				</div>
-			</div>
+			{loading ? (
+				"loading"
+			) : (
+				<>
+					{data &&
+						images.map((img, i) => (
+							<div className="pListItem" key={i}>
+								<div className="pListItem">
+									<img src={img} alt="hotel" className="pListImg" />
+									<div className="pListTitles">
+										<h1>{data[i]?.type}</h1>
+										<h2>
+											{data[i]?.count} {data[i]?.type}
+										</h2>
+									</div>
+								</div>
+							</div>
+						))}
+				</>
+			)}
 		</div>
 	);
 };
