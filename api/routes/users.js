fc240151b0d19ac3d730095895e96653522f08ca
jiahoe <mrjiahoe@hotmail.com>
2022-06-08 23:20:40 +0800

    login comment for future me

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,6 +12,7 @@ const router = express.Router();
 // router.get("/checkauthentication", verifyToken, (req, res, next) => {
 // 	res.send("hello user, you are logged in");
 // });
+// inside .env, need MONGO and JWT, jwt is from "openssl rand -base64 32" in console
 
 // router.get("/checkuser/:id", verifyUser, (req, res, next) => {
 // 	res.send("hello user, you are logged in and you can delete your account");
